d733c8cb4026877836c48d5a43322e0651bddb98
Denis Seliverstov <sfaiross@yandex.ru>
2025-03-23 00:32:51 +0300

    Added mock page

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,12 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       <h1>Test page</h1>
-
+        <h1 className={styles.title}>Boxy</h1>
+        <p className={styles.subtitle}>Скоро здесь появится ваши коробочки...</p>
+        <button className={styles.button}>Добавить коробочку</button>
       </main>
       <footer className={styles.footer}>
+        <p>© 2025 Boxy. Все права защищены.</p>
       </footer>
     </div>
   );
